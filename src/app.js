6c570b5946bb44c4fec6fea6b0140712a3604c11
averigibson <85973265+averigibson@users.noreply.github.com>
2021-08-15 20:32:17 -0500

    Made Search Bar Work

--- src/app.js
+++ src/app.js
@@ -42,9 +42,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "06302d11959f00f29b052ca84aa10395";
-let city = "Nashville";
-let apiUrl = `
-            https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "06302d11959f00f29b052ca84aa10395";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
